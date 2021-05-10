ebd7478eed9b32ad62c8fd7daf965bebc23bc76b
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-10 10:12:34 +0300

    fetch

--- js/api.js
+++ js/api.js
@@ -2,23 +2,35 @@
 console.log("api");
 
 const loadBtn = document.getElementById("load");
+const loadMore = document.getElementById("load-more");
+const appEl = document.getElementById("app");
+
+let postKartotinis = 1;
 
 loadBtn.addEventListener("click", getUsers);
+loadMore.addEventListener("click", getMorePosts);
 
 // gauti 100 useriu duomenis is nutolio serverio json formatu
-function getUsers() {
+function getUsers(event, howMany = 1) {
   fetch("https://jsonplaceholder.typicode.com/posts")
     //                json() - nes gaunam duomenis json formatu
     .then((resp) => resp.json())
     // gaunam duomenis
     .then((data) => {
-      generatePosts(data);
+      console.log(data);
+
+      // visi 100postu yra data
+      //   console.log(data);
+      //   debugger;
+      generatePosts(data.slice(0, 3 * howMany));
+      //   generatePosts(data);
     })
     // jei yra klaidu jos bus atvaizduotos catch dalyje
     .catch((err) => console.warn(err));
 }
 
 function generatePosts(postData) {
+  console.log(postData);
   let str = "";
   postData.forEach((post) => {
     str += `
@@ -26,9 +38,14 @@ function generatePosts(postData) {
           <h3>${post.title}</h3>
           <p>${post.body}</p>
           <button class='open-modal'>Read more</button> 
-          <span>userId: ${post.userId}</span>
+          <span>id: ${post.id}</span>
       </article>
         `;
   });
-  document.getElementById("app").innerHTML = str;
+  appEl.innerHTML = str;
+}
+
+function getMorePosts() {
+  console.log("get more posts");
+  getUsers(null, ++postKartotinis);
 }
